6abbacb2cb086a77f33402e785b6ee1f90d81329
Mit Sudebov <sudebovds@gmail.com>
2020-12-08 19:48:08 +0300

    First markup and logic for Sample page

--- src/pages/sample/Sample.js
+++ src/pages/sample/Sample.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RateContext } from '../../context/RateContext';
+import { Button } from '../../components/button/Button';
 import './sample.scss';
 
 export const Sample = () => {
 
+    const { state } = useContext(RateContext);
+
     return(
-        <div>
-            <h1>Samples</h1>
+        <div className = 'sample'>
+            <div className = "sampleContainer">
+                <div>
+                    <h3>
+                        Get the cours: &nbsp;
+
+                        <select>
+                            {
+                                Object.keys(state.currency)
+                                    .map((item, i) => {
+                                        return(
+                                            <option key = {item}>
+                                                {item}
+                                            </option>
+                                        );
+                                    })
+                            }
+                        </select>
+
+                        &nbsp;&nbsp; to &nbsp;&nbsp;
+
+                        <select>
+                            {
+                                Object.keys(state.currency)
+                                    .map((item, i) => {
+                                        return(
+                                            <option key = {item}>
+                                                {item}
+                                            </option>
+                                        );
+                                    })
+                            }                            
+                        </select>
+
+                    </h3>
+                </div>
+                <div className = 'sampleHead'>
+                    <span>Date: <input type = 'date' /></span>
+
+                    <Button text = 'Get cours'/>
+                </div>
+                <div className = 'smapleResult'>
+                    <ul>
+
+                    </ul>
+                </div>
+            </div>            
         </div>
     );
 }
